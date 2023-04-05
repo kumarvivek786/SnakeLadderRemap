3f35511d67e2d64e02ff47ededda5bbd5ff2a35e
vivek <ikumarvivek786@gmail.com>
2023-04-05 21:56:29 +0530

    [Vivek]Player Check Options]

--- SnakeAndLadderRemap/Program.cs
+++ SnakeAndLadderRemap/Program.cs
@@ -10,11 +10,33 @@ namespace SnakeAndLadderRemap
     {
         static void Main(string[] args)
         {
-            //UC-2 Roll Die
-            int Start_Position = 0, r = 0;
+            int Start_Position = 0, r = 0, choice = 0;
+            const int NOPLAY = 0;
+            const int LADDER = 1;
+            const int SNAKE = 2;
             Random random = new Random();
             r = random.Next(1, 7);
             Console.WriteLine("Dice:" + r);
+
+            choice = random.Next(0, 3);
+            Console.WriteLine("choice: {0}", choice);
+
+            switch (choice)
+            {
+                case NOPLAY:
+                    Console.WriteLine("position: {0}", Start_Position);
+                    break;
+                case LADDER:
+                    Start_Position = Start_Position + r;
+                    Console.WriteLine("position: {0}", Start_Position);
+                    break;
+                case SNAKE:
+                    Start_Position = Start_Position - r;
+                    if (Start_Position < 0)
+                        Start_Position = 0;
+                    Console.WriteLine("position:{0}", Start_Position);
+                    break;
+            }
             Console.ReadLine();
         }
     }
