0a4d07f119859f2bd47293ba3c2af2b20f6abf7f
vivek <ikumarvivek786@gmail.com>
2023-04-05 21:44:33 +0530

    [Vivek]Player At Start Position]

---
+++ SnakeAndLadderRemap/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderRemap
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC-1 single player at start position 0
+            int Start_Position = 0;
+            Console.ReadLine();
+        }
+    }
+}
